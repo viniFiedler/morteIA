cf0cc2502a6804d2eefae1718da9b4cc2e4af380
SpecTro <natplayspectro@gmail.com>
2024-07-21 17:57:23 -0300

    create view and render function

--- Server/index.php
+++ Server/index.php
@@ -2,12 +2,16 @@
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
-require "./server.php";
+/**
+ * Render the prompt view
+ *
+ * @param string $prompt
+ * @param string $playerName
+ * @param string|false $input
+ * @return void
+ */
+function render($prompt, $playerName = "", $input = "") {
+    require "../Client/prompt-view.php";
+}
 
-
-?>
-
-
-<html>
-<div> NAO EH PISHING </div>
-</html>
+require "server.php";
